c4d33b60483589d21ba826e46d66a15aface6dfa
Robin Altman <robinhaltman@gmail.com>
2023-04-16 00:00:58 -0400

    Basically there...

--- script.js
+++ script.js
@@ -39,3 +39,14 @@ function toggle(id) {
     hiddenDiv.style.display = "none";
   }
 }
+
+
+//POPUPPOPUPPOPUPPOPUPPOPUPPOPUPPOPUPPOPUPPOPUPPOPUP
+
+setTimeout(function() {
+  document.getElementById("popup").classList.remove("hidden");
+}, 2000);
+
+document.getElementById("close-popup").addEventListener("click", function() {
+  document.getElementById("popup").classList.add("hidden");
+});
